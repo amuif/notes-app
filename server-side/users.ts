b8560013015deb889d839db8db29a488c761a76b
amuif <amudihayat@gmail.com>
2025-07-30 17:45:47 +0300

    added auth,db

---
+++ server-side/users.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { auth } from "@/lib/auth";
+
+export const SignInUser = async ({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) => {
+  try {
+    await auth.api.signInEmail({
+      body: {
+        email,
+        password,
+      },
+      asResponse: true,
+    });
+
+    return { success: true, message: "Signed in successfully " };
+  } catch (error) {
+    const e = error as Error;
+    return { success: false, message: e.message || "Failed to signin" };
+  }
+};
+export const SignUpUser = async ({
+  email,
+  password,
+  name,
+}: {
+  email: string;
+  password: string;
+  name: string;
+}) => {
+  try {
+    await auth.api.signUpEmail({
+      body: {
+        email,
+        password,
+        name,
+      },
+      asResponse: true,
+    });
+
+    return { success: true, message: "Signed up successfully " };
+  } catch (error) {
+    const e = error as Error;
+    return { success: false, message: e.message || "Failed to signin" };
+  }
+};
